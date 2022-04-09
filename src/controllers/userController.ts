dbc477386ce609699165c2945b70f0f6b8098f60
Samuel Durante <samueldurante.h@gmail.com>
2022-04-09 21:12:47 +0000

    feat: implement login router

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,26 @@
 import { Request, Response } from 'express'
 import argon2 from 'argon2'
+import jwt from 'jsonwebtoken'
 
 import userRepository from '../repositories/userRepository'
 
-export const login = async (_: Request, response: Response) => {
-  return response.status(200).json({ foo: 'bar' })
+export const login = async (request: Request, response: Response) => {
+  const { username, password } = request.body
+
+  const user = await userRepository.findByUsername(username)
+
+  if (!user) {
+    return response.status(404).json({ error: 'AccountNotExists' })
+  }
+
+  if (!(await argon2.verify(user.password, password))) {
+    return response.status(401).json({ error: 'IncorrectPassword' })
+  }
+
+  const privateKey = process.env.PRIVATE_KEY as string
+  const token = jwt.sign({ id: user.id }, privateKey)
+
+  return response.status(200).json({ token })
 }
 
 export const register = async (request: Request, response: Response) => {
